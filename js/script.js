d30106979440e5424e24f88cf3cbb6ff9f3a0b55
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-02-03 13:45:27 -0300

    estilizando cartas- e adicionando rotação

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 let qtd_cartas = 0;
+const cards = document.querySelector(".cards");
 
 while( qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2 != 0 ) {
     qtd_cartas = prompt("Digite um número par de cartas entre 4 e 14");
     qtd_cartas = parseInt(qtd_cartas);
 }
 
+const imagens = [
+    'img0.gif',
+    'img1.gif',
+    'img2.gif',
+    'img3.gif',
+    'img4.gif',
+    'img5.gif',
+    'img6.gif',
+]
+
+let cartas = [];
+contador=0;
+for (let i = 0 ; i < qtd_cartas ; i += 2) {
+    cartas.push(imagens[contador]);
+    cartas.push(imagens[contador]);
+    contador ++;
+}
+
+let codigoHTML = '';
+cartas.forEach(img =>{
+    codigoHTML += `    
+        <div class="memory-card">
+            <img class="frente-carta" src="img/${img}">
+            <img class="fundo-carta" src="img/front.png">
+        </div>        
+    `;
+});
+
+cards.innerHTML = codigoHTML;
+
+const pegarcartas = document.querySelectorAll(".memory-card");
+
+function virar(){
+    this.classList.add("virar")
+}
+pegarcartas.forEach(c => c.addEventListener('click',virar));
