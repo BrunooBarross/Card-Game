0ebf651e22b2ba131db756e3d611f4eebcc374a3
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-02-06 17:59:41 -0300

    adicionando regra para reiniciar o jogo ou fechar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-let qtd_cartas = 0;
+let qtd_cartas;
+
 const cards = document.querySelector(".cards");
 
-while( qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2 != 0 ) {
-    qtd_cartas = prompt("Digite um número par de cartas entre 4 e 14");
-    qtd_cartas = parseInt(qtd_cartas);
-}
 const imagens = [
     'img0.gif',
     'img1.gif',
@@ -20,28 +17,36 @@ imagens.sort( function () {
 });
 
 let cartas = [];
-contador=0;
-for (let i = 0 ; i < qtd_cartas ; i += 2) {
-    cartas.push(imagens[contador]);
-    cartas.push(imagens[contador]);
-    contador ++;
+
+iniciarJogo();
+function iniciarJogo(){
+    qtd_cartas = 0;
+    while( qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2 != 0 ) {
+        qtd_cartas = prompt("Digite um número par de cartas entre 4 e 14");
+        qtd_cartas = parseInt(qtd_cartas);
+    }    
+    contador=0;
+    for (let i = 0 ; i < qtd_cartas ; i += 2) {
+        cartas.push(imagens[contador]);
+        cartas.push(imagens[contador]);
+        contador ++;
+    }
+
+    cartas.sort( function () {
+        return 0.5 - Math.random();
+    });
+
+    let codigoHTML = '';
+    cartas.forEach(img =>{
+        codigoHTML += `    
+            <div class="memory-card" onclick="virar(this)" data-carta="${img}">
+                <img class="frente-carta" src="img/${img}">
+                <img class="fundo-carta" src="img/front.png">
+            </div>        
+        `;
+    });
+    cards.innerHTML = codigoHTML;
 }
-
-cartas.sort( function () {
-    return 0.5 - Math.random();
-});
-
-let codigoHTML = '';
-cartas.forEach(img =>{
-    codigoHTML += `    
-        <div class="memory-card" onclick="virar(this)" data-carta="${img}">
-            <img class="frente-carta" src="img/${img}">
-            <img class="fundo-carta" src="img/front.png">
-        </div>        
-    `;
-});
-
-cards.innerHTML = codigoHTML;
 
 let primeira = null;
 let segunda;
@@ -101,6 +106,7 @@ function remover(){
     //     bloqueio = false;            
     // },1000);    
 }
+
 function reset(){
     bloqueio = false;
     primeira.setAttribute("onClick","");
@@ -112,7 +118,19 @@ function reset(){
         if(contafinalizar *2 === qtd_cartas){            
             alert ("Você ganhou em "+ contacliques +" jogadas!"
             + "\n"+"Tempo de jogo: "+valorRelogio+" segundos"); 
-            clearInterval(intervaloRelogio);  
+            clearInterval(intervaloRelogio);
+            intervaloRelogio = null;  
+            const jogar = prompt("Gostaria de jogar novamente? S/N");
+            if(jogar === "sim" || jogar === "Sim" || jogar === "s" || jogar == "S"){
+                relogio.innerHTML = 0;
+                contafinalizar = 0;
+                cartas.splice(0, cartas.length);
+                iniciarJogo();
+            }else if(jogar === "nao" || jogar === "Nao" || jogar === "n" || jogar == "N" || jogar === "não" || jogar === "Não"){
+                window.close();
+            }else{
+                jogar = prompt('Por favor! Digite: "Sim" ou "Não"');
+            }            
         }
     },100);  
 }
